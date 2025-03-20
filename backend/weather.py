562904e75212f9dabcebd800f1a97fc82398d9c5
Rashadc72 <Rashadcoggins72@gmail.com>
2025-03-20 15:26:03 -0500

    Done

--- backend/weather.py
+++ backend/weather.py
@@ -1,4 +1,16 @@
 import requests
 from dotenv import load_dotenv
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass 
+
+API_key = '10b20d494de92c5f001630a24ff98007'
+
+def get_weather_data(city_name, state_code, country_code, API_key):
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{state_code},{country_code}&appid={API_key}&units=imperial'
+    response = requests.get(url)
+    data = response.json()
+    weather = data.get('weather')
+    return weather
+print(get_weather_data('nashville', 'TN', 'US', API_key))
+
+    
