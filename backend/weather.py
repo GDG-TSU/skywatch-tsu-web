393eb119ea9824c44578994479f6217e4246f813
KB010306 <kimanibernard447@gmail.com>
2025-03-20 16:22:15 -0500

    Update weather.py
    Created a function to retrieve the weather from the api, removed the api key (import requests doesn't work for me on vscode but i will fix it. the alternative method does though)

--- backend/weather.py
+++ backend/weather.py
@@ -6,4 +6,4 @@ def getweather(city, state, country, apikey):
     response = pip._vendor.requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city},{state},{country}&appid={apikey}')
     return response.json()
 
-pprint.pprint(getweather('Nashville', 'TN', 'US', '[APIKEY'))
+pprint.pprint(getweather('Nashville', 'TN', 'US', '[APIKEY]'))
