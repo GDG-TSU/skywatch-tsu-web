95e7766f7e7daa74be616426cc70e30258f034ad
Cword42073 <Cword42073@gmail.com>
2025-03-24 10:56:41 -0500

    Added changes from Rashad Branch & moved API Key to .env

--- backend/weather.py
+++ backend/weather.py
@@ -3,7 +3,12 @@ from dotenv import load_dotenv
 import os
 from dataclasses import dataclass 
 
-API_key = '10b20d494de92c5f001630a24ff98007'
+load_dotenv()
+api_key = os.getenv('API_KEY')
+
+@dataclass
+class WeatherData:
+    main: str
 
 def get_weather_data(city_name, state_code, country_code, API_key):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{state_code},{country_code}&appid={API_key}&units=imperial'
@@ -12,9 +17,18 @@ def get_weather_data(city_name, state_code, country_code, API_key):
 
     main = data.get('main')
     weather = data.get('weather')
+    wind = data.get('wind')
+    sys = data.get('sys')
 
-    return main
-
-print(get_weather_data('nashville', 'TN', 'US', API_key))
-
-    
+    return {
+        'main' : weather[0].get('main'),
+        'description' : weather[0].get('description'),
+        'temp' : main.get('temp'),
+        'feels_like' : main.get('feels_like'),
+        'humidity' : main.get('humidity'),
+        'temp_min' : main.get('temp_min'),
+        'temp_max' : main.get('temp_max'),
+        'speed' : wind.get('speed'),
+        'name' : data.get('name'),
+        'country' : sys.get('country')
+    }
